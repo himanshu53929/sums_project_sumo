4ea2ad983a844f56895654f4556dfb13a7eab6b1
himanshu53929 <himanshuchandthakuri@gmail.com>
2026-01-19 08:09:13 +0545

    Some Improvements done such as every kind of vehicles appear in each lane, and taxi was removed

--- run.py
+++ run.py
@@ -21,41 +21,58 @@ def lock_lane_changes():
     for veh_id in traci.vehicle.getIDList():
         traci.vehicle.setLaneChangeMode(veh_id, 0)
 
-# Duration
-small_traffic = "10"
-medium_traffic = "20"
-large_traffic = "30"
+# # Duration
+# small_traffic = "10"
+# medium_traffic = "20"
+# large_traffic = "30"
 
-# State
-horizontal_lane = "GGGrrGGrrrGGGrrGGrrr"
-first_buffer = "GGyrrGGrrrGGyrrGGrrr"
+# # State
+# horizontal_lane = "GGGrrGGrrrGGGrrGGrrr"
+# first_buffer = "GGyrrGGrrrGGyrrGGrrr"
 
-vertical_lane = "GGrrrGGGrrGGrrrGGGrr"
-second_buffer = "GGrrrGGyrrGGrrrGGyrr"
+# vertical_lane = "GGrrrGGGrrGGrrrGGGrr"
+# second_buffer = "GGrrrGGyrrGGrrrGGyrr"
 
-horizontal_diagonal_lane = "GGrrGGGrrrGGrrGGGrrr"
-third_buffer = "GGrryGGrrrGGrrGGyrrr"
+# horizontal_diagonal_lane = "GGrrGGGrrrGGrrGGGrrr"
+# third_buffer = "GGrryGGrrrGGrrGGyrrr"
 
-vertical_diagonal_lane = "GGrrrGGrrGGGrrrGGrrG"
-fourth_buffer = "GGrryGGrrrGGrryGGrrr"
+# vertical_diagonal_lane = "GGrrrGGrrGGGrrrGGrrG"
+# fourth_buffer = "GGrryGGrrrGGrryGGrrr"
 
-# List of count of vehicles which comes from yolo model
-traffic_count = []
+# # List of count of vehicles which comes from yolo model
+# traffic_count = []
 
 tl_id = "middle_junction"
-previous_state = traci.trafficlight.getRedYellowGreenState(tl_id)
-print("Initial traffic light state of ", tl_id, "is ", previous_state)
+# previous_state = traci.trafficlight.getRedYellowGreenState(tl_id)
+# print("Initial traffic light state of ", tl_id, "is ", previous_state)
 
 while traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep()
+
+    state = traci.trafficlight.getRedYellowGreenState(tl_id)
+    links = traci.trafficlight.getControlledLinks(tl_id)
+
+    for veh in traci.vehicle.getIDList():
+        lane = traci.vehicle.getLaneID(veh)
+
+        if not lane.endswith("_0"):
+            continue
+
+        for i, group in enumerate(links):
+            for fromLane, toLane, via in group:
+                if lane == fromLane and state[i] == "G":
+                    traci.vehicle.setSpeedMode(veh, 32)
+                    traci.vehicle.setSpeed(veh, traci.lane.getMaxSpeed(lane))
+
+
     lock_lane_changes()
 
-    current_state = traci.trafficlight.getRedYellowGreenState(tl_id)
+    # current_state = traci.trafficlight.getRedYellowGreenState(tl_id)
 
-    if(previous_state != current_state):
-        phase_index = traci.trafficlight.getPhase(tl_id)
-        phase_duration = traci.trafficlight.getPhaseDuration(tl_id)
-        print(f"Current traffic light state of {tl_id} is {current_state} it has phase index of {phase_index} and it has phase duration of {phase_duration}")
-        previous_state = current_state
+    # if(previous_state != current_state):
+    #     phase_index = traci.trafficlight.getPhase(tl_id)
+    #     phase_duration = traci.trafficlight.getPhaseDuration(tl_id)
+    #     print(f"Current traffic light state of {tl_id} is {current_state} it has phase index of {phase_index} and it has phase duration of {phase_duration}")
+    #     previous_state = current_state
 
 traci.close()
